3c7be8c30e77e576a92ae37baa97fd4fbea7d269
Prisha <prishaverma458@gmail.com>
2025-06-26 19:04:02 +0530

    Patterns5 solved

---
+++ Lec2_Patterns5.java
@@ -0,0 +1,13 @@
+public class Lec2_Patterns5 {
+        public static void seeding(int n) {
+        // Write your code here
+        for(int i=n;i>0;i--){
+            for(int j=i;j>0;j--){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
+    
+
