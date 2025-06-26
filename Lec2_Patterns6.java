c12e6177b2545ea9a1f332c04e7db3898ed9f666
Prisha <prishaverma458@gmail.com>
2025-06-26 23:45:24 +0530

    Patterns6 solved

---
+++ Lec2_Patterns6.java
@@ -0,0 +1,17 @@
+public class Lec2_Patterns6 {
+    public static void nNumberTriangle(int n) {
+        // Write your code here
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=n-i+1;j++){
+                System.out.print(j+" ");
+                
+
+            
+
+            }
+            System.out.println();
+        }
+    }
+}
+    
+
