1e9cd6499856bbf05cca1605b77a629956b43edc
Prisha <prishaverma458@gmail.com>
2025-06-27 09:15:59 +0530

    applied different logic in pattern6

--- Lec2_Patterns6.java
+++ Lec2_Patterns6.java
@@ -1,9 +1,9 @@
 public class Lec2_Patterns6 {
     public static void nNumberTriangle(int n) {
         // Write your code here
-        for(int i=1;i<=n;i++){
-            for(int j=1;j<=n-i+1;j++){
-                System.out.print(j+" ");
+        for(int i=n;i>0;i--){
+            for(int j=0;j<i;j++){
+                System.out.print(j+1+" ");
                 
 
             
@@ -13,5 +13,6 @@ public class Lec2_Patterns6 {
         }
     }
 }
+
     
 
