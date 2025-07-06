c2c26d94689ccbe43ccb76c07d65c53fda97bc29
Prisha <prishaverma458@gmail.com>
2025-07-06 14:47:45 +0530

    Diamond pattern

---
+++ Lec9_Patterns9.java
@@ -0,0 +1,38 @@
+public class Lec9_Patterns9 {
+        public static void nStarDiamond(int n) {
+            // Write your code here
+            
+              int sp = n - 1;
+            int st = 1;
+    
+            for(int i=0;i<n;i++){
+                for(int j=0;j<sp;j++){
+                    System.out.print(" ");
+                }
+                for(int k=0;k<st;k++){
+                    System.out.print("*");
+                }
+               
+                System.out.println();
+                sp--;
+                st=st+2;
+            }
+            sp=0;
+            st=n*2-1;
+            for(int j=0;j<n;j++){
+                for(int i=0;i<sp;i++){
+                    System.out.print(" ");
+                }
+                for(int k=0;k<st;k++){
+                    System.out.print("*");
+                }
+                System.out.println();
+                sp++;
+                st=st-2;
+    
+            }
+            
+        }
+    }
+    
+
