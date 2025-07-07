886e858ab80f98f3dafa7b8dcd08f6df474bbae7
Prisha <prishaverma458@gmail.com>
2025-07-07 21:36:19 +0530

    patterns2 solved

--- Lec2_Patterns2.java
+++ Lec2_Patterns2.java
@@ -1,3 +1,37 @@
+/*Problem statement
+Sam is making a forest visualizer. An N-dimensional forest is represented by the pattern of size NxN filled with ‘*’.
+
+An N/2-dimensional forest is represented by the lower triangle of the pattern filled with ‘*’.
+
+For every value of ‘N’, help sam to print the corresponding N/2-dimensional forest.
+
+Example:
+Input:  ‘N’ = 3
+
+Output: 
+* 
+* *
+* * *
+Detailed explanation ( Input/output format, Notes, Images )
+Constraints :
+1  <= N <= 25
+Time Limit: 1 sec
+Sample Input 1:
+3
+Sample Output 1:
+* 
+* *
+* * *
+Explanation Of Sample Input 1 :
+For N = 3, fill all the rows and columns in the lower triangle of 3x3 matrix with ‘*’.
+Sample Input 2 :
+1
+Sample Output 2 :
+* 
+
+*/
+
+
 public class Lec2_Patterns2 {
     public static void nForest(int n) {
         // Write your code here
