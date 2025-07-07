72957fabb978cc52d38495ebb8bfd81377baebbe
Prisha <prishaverma458@gmail.com>
2025-07-07 21:23:00 +0530

    patterns9 solved

--- Lec2_Patterns9.java
+++ Lec2_Patterns9.java
@@ -1,4 +1,40 @@
-public class Lec9_Patterns9 {
+/*Problem statement
+Ninja was very fond of patterns. For a given integer ‘N’, he wants to make the N-Star Diamond.
+
+Example:
+Input: ‘N’ = 3
+
+Output: 
+
+  *
+ ***
+*****
+*****
+ ***
+  *
+Detailed explanation ( Input/output format, Notes, Images )
+Constraints :
+1  <= N <= 20
+Time Limit: 1 sec
+Sample Input 1:
+3
+Sample Output 1:
+  *
+ ***
+*****
+*****
+ ***
+  *    
+Sample Input 2 :
+1
+Sample Output 2 :
+*
+*
+*/
+
+
+
+public class Lec2_Patterns9 {
         public static void nStarDiamond(int n) {
             // Write your code here
             
