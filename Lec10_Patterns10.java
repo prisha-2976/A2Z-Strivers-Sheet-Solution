b850c38fbffb116e636f072424882e4ab4db05f4
Prisha <prishaverma458@gmail.com>
2025-07-07 21:17:45 +0530

    patterns10 solved

--- Lec10_Patterns10.java
+++ Lec10_Patterns10.java
@@ -1,3 +1,33 @@
+/*  Problem statement
+Ninja was very fond of patterns. For a given integer ‘N’, he wants to make the N-Star Rotated Triangle.
+
+Example:
+Input: ‘N’ = 3
+
+Output: 
+
+*
+**
+***
+**
+*
+Detailed explanation ( Input/output format, Notes, Images )
+Constraints :
+1  <= N <= 20
+Time Limit: 1 sec
+Sample Input 1:
+3
+Sample Output 1:
+*
+**
+***
+**
+*
+Sample Input 2 :
+1
+Sample Output 2 :
+*/
+
 public class Lec10_Patterns10 {
         public static void nStarTriangle(int n) {
             // Write your code here
