ee889e6ff4f1a61d44e99eda56081dca12148341
Prisha <prishaverma458@gmail.com>
2025-07-13 23:01:52 +0530

    Patterns13 solved

---
+++ Lec2_Patterns13.java
@@ -0,0 +1,15 @@
+public class Lec2_Patterns13 {
+    public static void nNumberTriangle(int n) {
+        // Write your code here
+        int cnt=0;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<=i;j++){
+                System.out.print(cnt+1+" ");
+                cnt++;
+            }
+            System.out.println();
+        }
+    }
+}
+    
+
