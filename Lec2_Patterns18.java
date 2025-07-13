97dda7ffd01a8482dc913e1747572bf28330d630
Prisha <prishaverma458@gmail.com>
2025-07-14 00:37:41 +0530

    Patterns18 solved

---
+++ Lec2_Patterns18.java
@@ -0,0 +1,16 @@
+public class Lec2_Patterns18 {
+     public static void alphaTriangle(int n) {
+        // Write your code here
+               char start = (char)('A' + n - 1); 
+        for(int i=0;i<n;i++){
+            char ch = start;
+            for(int j=0;j<=i;j++){
+                System.out.print(ch + " ");
+                ch--;
+            }
+            System.out.println();
+        }
+    }
+}
+    
+
