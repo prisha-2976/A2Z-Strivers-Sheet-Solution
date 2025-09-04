b0e46621652a913b1067044080fa5d7bd47a452a
Prisha <prishaverma458@gmail.com>
2025-09-04 13:08:23 +0530

    recursive and iterative approach

--- Begin Journey/ImportantSortingTechniques/InsertionSort.java
+++ Begin Journey/ImportantSortingTechniques/InsertionSort.java
@@ -24,5 +24,31 @@ public class InsertionSort {
             System.out.print(num + " ");
         }
     }
-    
 }
+// Recursion way
+/* 
+void insertionSortRecursive(int[] arr, int n) {
+    // Base case: If the array size is 1 or less, it's already sorted
+    if (n <= 1) { // N==0
+        return;
+    }
+
+    // Sort the first n-1 elements
+    insertionSortRecursive(arr, n - 1);
+    int j=n;
+
+   
+
+    // Move elements of arr[0..n-1], that are greater than last,
+    // to one position ahead of their current position
+    while (j > 0 && arr[j-1] > arr[j]) {
+       int temp = arr[j - 1];
+       arr[j - 1] = arr[j];
+        arr[j] = temp;
+        j--;
+    }
+   
+}
+// Can't reduce space below O(n) unless you go iterative
+// Time Complexity: O(N^2)- average and worst case, O(N)- best case
+// Space Complexity: O(N) due to call stack space. */
